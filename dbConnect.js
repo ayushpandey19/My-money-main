554242c7ca4c0222b0ddb399008c8b52b0f07858
ayushpandey19 <pandeyayush191202@gmail.com>
2024-09-08 14:11:42 +0530

    Initial commit

---
+++ dbConnect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://Dhruv:Rajan%4072@cluster0.5jroi.mongodb.net/my-money',
+{ useNewUrlParser: true , useUnifiedTopology: true })
+
+.then(()=>{ return console.log("Connected to MongoDB Localhost...");
+ })
+.catch(err => console.log("Could not connect",err))
+
+const connection = mongoose.connection
+
